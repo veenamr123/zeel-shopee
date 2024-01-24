4f5a53cac848510307768425950ad50d77516bd5
nash <nash@DESKTOP-GLAFMTS>
2024-01-24 17:26:33 +0530

    category is ready

---
+++ src/test/java/com/admin/CreateCategoryTest.java
@@ -0,0 +1,13 @@
+package com.admin;
+
+import org.testng.annotations.Test;
+
+public class CreateCategoryTest {
+	
+	@Test
+	public void CategoryTest() 
+	{
+		System.out.println(" category created ");
+	}
+
+}
